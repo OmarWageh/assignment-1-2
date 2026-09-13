0ed0df130ee925a053165d6d46e1ef2c136578a3
omar wageh <omarwagih815@gmail.com>
2026-09-13 15:06:15 +0300

    fix: handle division by zero

--- assignment2/submission/assignment/Program.cs
+++ assignment2/submission/assignment/Program.cs
@@ -9,6 +9,23 @@
             int num1 = int.Parse(Console.ReadLine());
             Console.Write("pleae Enter the Second Number: ");
             int num2 = int.Parse(Console.ReadLine());
+            var addition = num1 + num2;
+            var subtraction = num1 - num2;
+            var multiplication = num1 * num2;
+
+            Console.WriteLine($"The Addition Two Numbers are: {addition}");
+            Console.WriteLine($"The subtraction Two Numbers are: {subtraction}");
+            Console.WriteLine($"The multiplication Two Numbers are: {multiplication}");
+            if (num2 == 0)
+            {
+                Console.WriteLine("Cannot divide by zero.");
+
+            }
+            else
+            {
+                var division = num1 / num2;
+                Console.WriteLine($"The Division Two Numbers are: {division}");
+            }
         }
     }
 }
